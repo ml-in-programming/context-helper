33d6d972d048c05ff7d3d9fc99e305ac40c987ba
niksaz <nikitasazanovich@gmail.com>
2017-10-22 13:32:59 +0300

    Removed TokenTypes non-relevant to a context capture.

--- src/main/java/ru/spb/se/contexthelper/context/ContextQueryBuilder.java
+++ src/main/java/ru/spb/se/contexthelper/context/ContextQueryBuilder.java
@@ -1,17 +1,78 @@
 package ru.spb.se.contexthelper.context;
 
+import com.google.common.collect.Sets;
+import com.intellij.psi.JavaTokenType;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.TokenType;
+import com.intellij.psi.tree.IElementType;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /** Builds query from the {@link EventContext}. */
 public class ContextQueryBuilder {
   private final static int MAX_WORDS_FOR_QUERY = 5;
+
+  private final static Set<IElementType> meaninglessForContextTokenTypes =
+      Sets.newHashSet(
+          TokenType.WHITE_SPACE,
+
+          JavaTokenType.C_STYLE_COMMENT,
+          JavaTokenType.END_OF_LINE_COMMENT,
+
+          JavaTokenType.LPARENTH,
+          JavaTokenType.RPARENTH,
+          JavaTokenType.LBRACE,
+          JavaTokenType.RBRACE,
+          JavaTokenType.LBRACKET,
+          JavaTokenType.RBRACKET,
+          JavaTokenType.SEMICOLON,
+          JavaTokenType.COMMA,
+          JavaTokenType.DOT,
+          JavaTokenType.ELLIPSIS,
+          JavaTokenType.AT,
+
+          JavaTokenType.EQ,
+          JavaTokenType.GT,
+          JavaTokenType.LT,
+          JavaTokenType.EXCL,
+          JavaTokenType.TILDE,
+          JavaTokenType.QUEST,
+          JavaTokenType.COLON,
+          JavaTokenType.PLUS,
+          JavaTokenType.MINUS,
+          JavaTokenType.ASTERISK,
+          JavaTokenType.DIV,
+          JavaTokenType.AND,
+          JavaTokenType.OR,
+          JavaTokenType.XOR,
+          JavaTokenType.PERC,
+
+          JavaTokenType.EQEQ,
+          JavaTokenType.LE,
+          JavaTokenType.GE,
+          JavaTokenType.NE,
+          JavaTokenType.ANDAND,
+          JavaTokenType.OROR,
+          JavaTokenType.PLUSPLUS,
+          JavaTokenType.MINUSMINUS,
+          JavaTokenType.LTLT,
+          JavaTokenType.GTGT,
+          JavaTokenType.GTGTGT,
+          JavaTokenType.PLUSEQ,
+          JavaTokenType.MINUSEQ,
+          JavaTokenType.ASTERISKEQ,
+          JavaTokenType.DIVEQ,
+          JavaTokenType.ANDEQ,
+          JavaTokenType.OREQ,
+          JavaTokenType.XOREQ,
+          JavaTokenType.PERCEQ,
+          JavaTokenType.LTLTEQ,
+          JavaTokenType.GTGTEQ,
+          JavaTokenType.GTGTGTEQ,
+
+          JavaTokenType.DOUBLE_COLON,
+          JavaTokenType.ARROW);
 
   private final EventContext context;
 
@@ -24,16 +85,17 @@ public class ContextQueryBuilder {
 
     Map<String, Integer> wordCountMap = new HashMap<>();
     for (PsiElement psiElement : psiElements) {
-      if (psiElement.getNode().getElementType() == TokenType.WHITE_SPACE) {
-        // Ignoring white spaces.
+      if (meaninglessForContextTokenTypes.contains(psiElement.getNode().getElementType())) {
         continue;
       }
       if (psiElement.getChildren().length > 0) {
-        // This is an abstract node, while for word counting we are concerned with concrete nodes.
+        // This is an abstract node, while for bag-of-words model we are only concerned with
+        // concrete nodes.
         continue;
       }
       if (psiElement.getTextLength() == 0) {
-        // This node's text is empty: won't help us with forming the request.
+        // The node's text representation is empty: won't help us with forming the query. E.g.
+        // REFERENCE_PARAMETER_LIST is present in PSI tree but its text may be empty.
         continue;
       }
       String elementText = psiElement.getText();
@@ -46,7 +108,6 @@ public class ContextQueryBuilder {
             .map(entry -> new WordInfo(entry.getKey(), entry.getValue()))
             .sorted(WordInfo.COMPARATOR_BY_TIMES_ENCOUNTERED)
             .collect(Collectors.toList());
-    // Taking words with the least encounters.
     return words.stream()
         .limit(MAX_WORDS_FOR_QUERY)
         .map(WordInfo::getText)
@@ -54,23 +115,23 @@ public class ContextQueryBuilder {
   }
 
   private static class WordInfo {
-    public static final Comparator<? super WordInfo> COMPARATOR_BY_TIMES_ENCOUNTERED =
-        Comparator.comparing(WordInfo::getTimesEncountered);
+    static final Comparator<? super WordInfo> COMPARATOR_BY_TIMES_ENCOUNTERED =
+        Comparator.comparing(WordInfo::getTimesEncountered).reversed();
 
     private String text;
 
     private int timesEncountered;
 
-    public WordInfo(String text, int timesEncountered) {
+    WordInfo(String text, int timesEncountered) {
       this.text = text;
       this.timesEncountered = timesEncountered;
     }
 
-    public String getText() {
+    String getText() {
       return text;
     }
 
-    public int getTimesEncountered() {
+    int getTimesEncountered() {
       return timesEncountered;
     }
   }

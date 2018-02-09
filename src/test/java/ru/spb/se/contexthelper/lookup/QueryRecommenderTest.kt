5799ee84d2155edef165427b359bfcae2196d46d
niksaz <nikitasazanovich@gmail.com>
2018-02-09 17:36:30 +0300

    Assured the quality of updated QueryRecommender.

---
+++ src/test/java/ru/spb/se/contexthelper/lookup/QueryRecommenderTest.kt
@@ -0,0 +1,40 @@
+package ru.spb.se.contexthelper.lookup
+
+import org.junit.Test
+import ru.spb.se.contexthelper.context.Keyword
+import ru.spb.se.contexthelper.context.Query
+
+class QueryRecommenderTest {
+    @Test
+    fun commonLookup() {
+        val recommender = QueryRecommender()
+        recommender.loadSuggestions(QUERIES_PATH)
+        val query = Query(
+            listOf(
+                Keyword("string", 2),
+                Keyword("split", 1)),
+            "Test query")
+        val questions = recommender.getRelevantQuestions(query, 5)
+        assert(questions.contains("Split letter string in java"))
+        assert(questions.contains("Split string from url pattern"))
+    }
+
+    @Test
+    fun camelCaseClassLookup() {
+        val recommender = QueryRecommender()
+        recommender.loadSuggestions(QUERIES_PATH)
+        val query = Query(
+            listOf(
+                Keyword("http", 2),
+                Keyword("get", 2),
+                Keyword("add", 1)),
+            "Test query")
+        val questions = recommender.getRelevantQuestions(query, 5)
+        assert(questions.contains("Add header to httpget request"))
+        assert(questions.contains("Add date header to httpget"))
+    }
+
+    companion object {
+        private const val QUERIES_PATH = "tasks/suggested.txt"
+    }
+}

7a7f3077cac6f7eb5aecc161961b7494d842e6cf
niksaz <nikitasazanovich@gmail.com>
2018-04-24 11:40:39 +0300

    Moved to apache.httpclient+jsoup.

--- src/main/java/ru/spb/se/contexthelper/lookup/GoogleSearchStackoverflowCrawler.kt
+++ src/main/java/ru/spb/se/contexthelper/lookup/GoogleSearchStackoverflowCrawler.kt
@@ -1,16 +1,18 @@
 package ru.spb.se.contexthelper.lookup
 
 import com.intellij.openapi.diagnostic.Logger
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.client.protocol.HttpClientContext
+import org.apache.http.client.utils.URIBuilder
+import org.apache.http.impl.client.HttpClients
+import org.jsoup.Jsoup
 import org.w3c.dom.Element
 import org.w3c.dom.Node
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.net.URL
-import java.net.URLEncoder
+import java.net.URI
 import java.util.*
-import java.util.regex.Pattern
-import javax.net.ssl.HttpsURLConnection
 import javax.xml.parsers.DocumentBuilderFactory
 
 class GoogleSearchStackoverflowCrawler : QuestionLookupClient {
@@ -60,50 +62,39 @@ class GoogleSearchStackoverflowCrawler : QuestionLookupClient {
 
     private fun lookupQuestionIdsStartingFrom(query: String, start: Int): List<Long> {
         Thread.sleep(WAIT_TIME_MIN_MS + randGenerator.nextInt(WAIT_TIME_SPREAD_MS))
+        val uri = constructUri(query, start)
+        LOG.info("crawler will issue HttpGet with $uri")
+        val htmlContent = getPageContent(uri)
+        val links = getLinksFromHtml(htmlContent)
+        return getStackOverflowQuestionIds(links)
+    }
+
+    private fun constructUri(query: String, start: Int): URI {
         val stackoverflowQuery = "site:stackoverflow.com $query"
-        val encodedQuery = URLEncoder.encode(stackoverflowQuery, "UTF-8")
-        val urlQuery =
-            "https://www.google.com/search?q=$encodedQuery&hl=en&gl=en" +
-                if (start != 0) "&start=$start" else ""
-        LOG.info("crawler issued urlQuery: $urlQuery")
-        val pageContent = getSearchContent(urlQuery)
-        val links = parseLinks(pageContent)
-        return links.mapNotNull {
-            // Format: https://stackoverflow.com/questions/id/...
-            val urlParts = it.split(Regex("/"))
-            try {
-                val questionIdText = urlParts[4]
-                questionIdText.toLong()
-            } catch (e: Exception) {
-                LOG.warn(e.message)
-                null
-            }
-        }.toList()
-    }
-
-    private fun getSearchContent(urlQuery: String): String {
-        val url = URL(urlQuery)
-        val connection = url.openConnection() as HttpsURLConnection
-        val agent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
-        connection.setRequestProperty("User-Agent", agent)
-        connection.setRequestProperty("Cookie", cookies)
-        val responseInputStream = connection.inputStream
-        return exhaustInputStream(responseInputStream)
-    }
-
-    private fun parseLinks(content: String): List<String> {
-        val links = mutableListOf<String>()
-        val pattern1 = "<h3 class=\"r\"><a href=\"/url?q="
-        val pattern2 = "\">"
-        val p = Pattern.compile(Pattern.quote(pattern1) + "(.*?)" + Pattern.quote(pattern2))
-        val matcher = p.matcher(content)
-        while (matcher.find()) {
-            var domainName = matcher.group(0).trim()
-            domainName = domainName.substring(domainName.indexOf("/url?q=") + 7)
-            domainName = domainName.substring(0, domainName.indexOf("&amp;"))
-            links.add(domainName)
+        val uriBuilder = URIBuilder()
+            .setScheme("https")
+            .setHost("www.google.com")
+            .setPath("/search")
+            .setParameter("q", stackoverflowQuery)
+            .setParameter("hl", "en")
+            .setParameter("gl", "en")
+        if (start != 0) {
+            uriBuilder.setParameter("start", "$start")
         }
-        return links.toList()
+        return uriBuilder.build()
+    }
+
+
+    private fun getPageContent(uri: URI): String {
+        val request = HttpGet(uri)
+        val agent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+        request.setHeader("User-Agent", agent)
+        request.setHeader("Cookie", cookies)
+        return HttpClients.createDefault().use { httpClient ->
+            httpClient.execute(request).use { response ->
+                exhaustInputStream(response.entity.content)
+            }
+        }
     }
 
     private fun exhaustInputStream(inputStream: InputStream): String {
@@ -118,13 +109,58 @@ class GoogleSearchStackoverflowCrawler : QuestionLookupClient {
         return builder.toString()
     }
 
+    private fun getLinksFromHtml(content: String): List<String> {
+        val links = mutableListOf<String>()
+        val doc = Jsoup.parse(content)
+        doc.select("h3").forEach { queryHeadElement ->
+            queryHeadElement.select("a").forEach { linkElement ->
+                var hrefValue = linkElement.attr("href")
+                hrefValue = hrefValue.substring(hrefValue.indexOf("/url?q=") + 7)
+                if (hrefValue.contains('&')) {
+                    hrefValue = hrefValue.substring(0, hrefValue.indexOf('&'))
+                }
+                links.add(hrefValue)
+            }
+        }
+        return links.toList()
+    }
+
+    private fun getStackOverflowQuestionIds(links: List<String>): List<Long> =
+        links.mapNotNull {
+            try {
+                var uri = URI(it)
+                var uriPathParts = uri.path.split('/')
+                // Redirect reference links to /questions/id/...
+                if (uriPathParts[1] != "questions") {
+                    val getMethod = HttpGet(it)
+                    HttpClients.createDefault().use { httpClient ->
+                        val context = HttpClientContext.create()
+                        httpClient.execute(getMethod, context).use {
+                            uri = context.redirectLocations.last()
+                            uriPathParts = uri.path.split('/')
+                        }
+                    }
+                }
+                if (uriPathParts[1] == "questions") {
+                    // Question path: /questions/id/...
+                    uriPathParts[2].toLong()
+                } else {
+                    LOG.warn("unknown path format for uri: $uri")
+                    null
+                }
+            } catch (e: Exception) {
+                LOG.warn(e.message)
+                null
+            }
+        }.toList()
+
     companion object {
         private val LOG = Logger.getInstance(GoogleSearchStackoverflowCrawler::class.java)
 
         private const val MAX_QUERIES = 1
         private const val RESULTS_PER_PAGE = 10
 
-        private const val WAIT_TIME_MIN_MS = 1 * 60 * 1000L
-        private const val WAIT_TIME_SPREAD_MS = 1 * 60 * 1000
+        private const val WAIT_TIME_MIN_MS = 1 * 5 * 1000L
+        private const val WAIT_TIME_SPREAD_MS = 1 * 5 * 1000
     }
 }
